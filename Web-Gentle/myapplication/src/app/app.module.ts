36d561486bffe478ef5dff7b671a2060c66c7c82
mmraihan <mm_raihan@live.com>
2022-01-21 13:23:12 +0600

    Remove Additional Components.

--- Web-Gentle/myapplication/src/app/app.module.ts
+++ Web-Gentle/myapplication/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './login/logout/logout.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    LogoutComponent
+
   ],
   imports: [
     BrowserModule
--- Web-Gentle/myapplication/src/app/home/home.component.ts
+++ Web-Gentle/myapplication/src/app/home/home.component.ts
@@ -2,8 +2,7 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  //templateUrl: './home.component.html',
-  template:`<h1>i am from temlate</h1>`,
+  templateUrl: './home.component.html', 
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
