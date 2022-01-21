56bb91a82af3d55c9eb5d4cb46f06c619e25a7c7
mmraihan <mm_raihan@live.com>
2022-01-21 14:13:08 +0600

    Text Interpolation.

--- Web-Gentle/myapplication/src/app/app.component.ts
+++ Web-Gentle/myapplication/src/app/app.component.ts
@@ -1,10 +1,27 @@
+import { newArray } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myapplication';
+  name: string = 'Mubasshir raihan';
+
+  getName(data: number): string {
+    if (data === 1) return 'Hello From ' + data;
+    else {
+      return 'Hello from with value ' + data;
+    }
+  }
+
+  employees = ['Emp-1', 'Emp-2'];
+
+  employee = {
+    name: 'Mubasshir Raihan',
+    title: 'Software Engineer',
+    src: "",
+    alt: "Pro Pic"
+  };
 }
