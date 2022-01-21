a11874d9ba2ee3b07589bb2d15af149ff12b2071
mmraihan <mm_raihan@live.com>
2022-01-21 12:58:03 +0600

    Template vs Template url.

--- Web-Gentle/myapplication/src/app/home/home.component.ts
+++ Web-Gentle/myapplication/src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  //templateUrl: './home.component.html',
+  template:`<h1>i am from temlate</h1>`,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
