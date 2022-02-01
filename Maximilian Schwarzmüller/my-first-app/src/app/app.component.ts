4ff8b3985d85854541168eb2d7ceab281ace8061
mmraihan <mm_raihan@live.com>
2022-02-01 15:16:29 +0600

    (65-57): Binding to Custom Properties.

--- Maximilian Schwarzmüller/my-first-app/src/app/app.component.ts
+++ Maximilian Schwarzmüller/my-first-app/src/app/app.component.ts
@@ -11,24 +11,6 @@ import { Component } from '@angular/core';
   `]
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName='';
-  newServerContent='';
-
-  onAddServer(){
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-  
-  onAddBlueprint(){
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
 }
