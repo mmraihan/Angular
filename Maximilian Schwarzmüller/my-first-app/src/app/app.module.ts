3ea293a62d7cb98c0b0669c9155b867b5914526d
mmraihan <mm_raihan@live.com>
2022-11-20 13:53:56 +0600

    Getting Closer to the Core of Observable.

--- Maximilian Schwarzmüller/my-first-app/src/app/app.module.ts
+++ Maximilian Schwarzmüller/my-first-app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -11,12 +14,15 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    UserComponent,
   
    
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
    
   ],
   providers: [],
