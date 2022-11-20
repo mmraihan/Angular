fcdda2b9c7cda2b5cee4f7c77d63cfdbb0887432
mmraihan <mm_raihan@live.com>
2022-11-20 13:14:23 +0600

    Clean up.

--- Maximilian Schwarzmüller/my-first-app/src/app/app.component.ts
+++ Maximilian Schwarzmüller/my-first-app/src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { UserService } from './services/user.service';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css'],
-  providers:[UserService]
+  providers:[]
  
 })
 export class AppComponent {
   
-constructor( private userService: UserService){
+constructor(){
 
 }
 }
--- Maximilian Schwarzmüller/my-first-app/src/app/user/user.ts
+++
@@ -1,6 +0,0 @@
-export class User{
-    name:string;
-    gender:string;
-    age:number;
- 
-}
